6d77f70a9d5729d2b5ad7eeeac36d89ef2bdd89d
Jennifer Patrick <118204029+jennifer-patrick@users.noreply.github.com>
2022-12-05 17:38:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ convertBtn.addEventListener("click", function() {
     `${number.value} meters = ${(number.value*3.281).toFixed(3)} feet | ${number.value} feet = ${(number.value/3.281).toFixed(3)} meters`
     
     volumeConvert.innerText=
-    `${number.value} litres = ${(number.value*1.7598).toFixed(3)} pints | ${number.value} litres = ${(number.value/1.7598).toFixed(2)} pints`
+    `${number.value} litres = ${(number.value*1.7598).toFixed(3)} pints | ${number.value} litres = ${(number.value/1.7598).toFixed(3)} pints`
     
     massConvert.innerText=
     `${number.value} kilograms = ${(number.value*2.204).toFixed(3)} pounds | ${number.value} kilograms = ${(number.value/2.204).toFixed(3)} pounds`
